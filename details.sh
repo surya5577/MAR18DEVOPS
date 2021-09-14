e50b8e0a8e56c611617bce57a762347b45a5011d
user1 <user1@ip-172-31-32-78.us-east-2.compute.internal>
2021-09-14 05:06:51 +0000

    added sudo code and user

--- details.sh
+++ details.sh
@@ -3,3 +3,5 @@ echo "this script will provide info about  the local system"
 echo $(whoami)
 echo $(ifconfig)
 echo ";;"
+# Gives the details 
+echo "Gives the user in the name as wel in the king of th"
